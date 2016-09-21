7713756b50e54923880cb5b71251dafa1139c52e
Dani Fernandez <daniel.fx.90@gmail.com>
2016-09-20 22:00:50 -0300

    Joystick declaration and implementation

---
+++ src/Joystick.h
@@ -0,0 +1,18 @@
+#ifndef Joystick_h
+#define Joystick_h
+
+#include "DigitalInput.h"
+#include "AnalogInput.h"
+
+class Joystick : public DigitalInput {
+
+	AnalogInput xAxis;
+	AnalogInput yAxis;
+
+public:
+	Joystick(int xAxisPin, int yAxisPin, int buttonPin);
+	AnalogInput& getXAxisInput();
+	AnalogInput& getYAxisInput();
+};
+
+#endif
