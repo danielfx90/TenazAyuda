32ab6b2014406cf0b6ca7f58f0574a36a25af95b
Dani Fernandez <daniel.fx.90@gmail.com>
2016-09-21 08:40:00 -0300

    Constants declaration in Configuration.h

---
+++ src/Configuration.h
@@ -0,0 +1,28 @@
+#ifndef Configuration_h
+#define Configuration_h
+
+/* ***************************************************************************************
+ *                                      PINES BOTONERA                                   *
+ * ***************************************************************************************/
+#define UP_BUTTON 2
+#define DOWN_BUTTON 4
+#define LEFT_BUTTON 5
+#define RIGHT_BUTTON 3
+#define START_BUTTON 6
+#define SELECT_BUTTON 7
+#define ANALOG_BUTTON 8
+#define X_AXIS A0
+#define Y_AXIS A1
+#define BUTTONS_QUANTITY 7
+
+/* ***************************************************************************************
+ *                                     MAPEO A FUNCIONES                                 *
+ * ***************************************************************************************/
+#define boton_pinza SELECT_BUTTON
+#define boton_palito DOWN_BUTTON
+#define boton_cambios ANALOG_BUTTON
+#define boton_grabarHacer LEFT_BUTTON
+#define boton_preseteado START_BUTTON
+#define boton_standby RIGHT_BUTTON
+
+#endif
